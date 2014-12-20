b9b5d2327cf8137845ec217d888a7a8d2844ff5f
Julien Hémono <jhemono@me.com>
2014-12-20 22:51:28 +0100

    Adding types, riddance of import, aerating the code

--- 038pandigitalmultiples.hs
+++ 038pandigitalmultiples.hs
@@ -1,6 +1,6 @@
 module PandigitalMultiples where
-import Data.List ((\\), nub, find, delete, unfoldr)
-import Control.Monad (mplus, mzero)
+import Data.List ((\\), find, delete, unfoldr)
+import Control.Monad (mplus, mzero, MonadPlus)
 
 ads0 :: [Int]
 ads0 = [1,2,3,4,5,6,7,8,9]
@@ -19,6 +19,7 @@ succdigital ads (d:ds) = first `mplus` second
           (ads' @ (d':_),ds') <- succdigital ads ds
           return (delete d' ads', d':ds')
 
+firstpandigital :: MonadPlus m => [t] -> m ([t], [t])
 firstpandigital [] = mzero
 firstpandigital (d:ads) = return (ads, [d])
 
@@ -31,6 +32,7 @@ range = unfoldr wrap n9
           do (_,a) <- succdigital ads0 n
              return (a,a)
 
+nb :: Int
 nb = length range
 
 order :: Int -> Int
@@ -38,11 +40,19 @@ order n = aux n 0
   where aux n' i
           | n' < 10 = i
           | otherwise = aux (n' `div` 10) (i+1)
+
+rrange :: [Pandigital]
 rrange = reverse range
+
+toint :: Pandigital -> Int
 toint = foldl (\acc d -> acc * 10 + d) 0
+
+taken :: Int -> Pandigital -> (Int, Pandigital)
 taken i xss = (n,xs)
   where n = toint p
         (p,xs) = splitAt i xss
+
+try :: Int -> Pandigital -> Bool
 try i pd = aux n0 pds
   where (n0,pds) = taken i pd
         aux _ [] = True
@@ -51,6 +61,8 @@ try i pd = aux n0 pds
                        (n'', xs') = taken o xs
                        in n' == n'' && aux n' xs'
 
+truc :: Pandigital -> Bool
 truc pd = any (`try` reverse pd) [1..4]
 
+answer :: Maybe Pandigital
 answer = find truc rrange
